6eb1309e83ca702226100b95b005336b22066730
unknown <mentor.manager.anissa@gmail.com>
2023-06-20 14:13:34 +0200

    dernier reglage

--- Backend/app.js
+++ Backend/app.js
@@ -14,10 +14,10 @@ const  limiteur  =  rateLimit ( {
 	legacyHeaders : false ,  // Désactive les en-têtes `X-RateLimit-*` 
 } )
 // Apply the rate limiting middleware to all requests
-app.use(limiteur)
+//app.use(limiteur)
 
 // Appliquer le middleware de limitation de débit à toutes les requêtes 
-app . utilisation ( limiteur )
+//app .utilisation ( limiteur )
 //Importations des routes
 const usersRoutes = require ('./routes/users.js');
 const saucesRoutes = require('./routes/sauce.js');
@@ -50,7 +50,7 @@ app.use(express.json());
 // Headers pour éviter les erreurs de CORS
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const router = express.Router();
 const userCtrl = require('../Controllers/user');
+const rateLimit = require ('express-rate-limit');
 
 router.post("/signup", userCtrl.signup);
 router.post("/login", userCtrl.login);
